35d0fb3096b7478bfa5ce7c6f1cce85c02536ea4
raviparmar0703 <105552565+raviparmar0703@users.noreply.github.com>
2023-02-14 17:11:56 +0530

    Add files via upload

--- student.component.ts
+++ student.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import {studentdata} from './student.model'
+import { ApiService } from '../sherd/api.service';
+import {HttpClient} from '@angular/common/http';
+
+
+
 
 @Component({
   selector: 'app-student',
@@ -9,9 +15,10 @@ import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 export class StudentComponent {
  showadd!:boolean;
  showupdate!:boolean;
+ studentmodelobj:studentdata=new studentdata
 
  formvalue!:FormGroup
- constructor(private formBuldier:FormBuilder){}
+ constructor(private formBuldier:FormBuilder,private api:ApiService){}
  
 
  ngOnInit():void{
@@ -23,9 +30,6 @@ export class StudentComponent {
 
   })
  }
-
-
-
   add(){
     this.showadd=true;
     this.showupdate=false;
@@ -35,5 +39,31 @@ export class StudentComponent {
 this.showadd=false;
 this.showupdate=true;
   }
+  addstudent(){
+    const myObserver = {
+      res: (res: number) => {
+        return console.log('Observer got a next value: ');
+      },
+      error: (err:any ) => console.error('Observer got an error: '),
+      
+    };
+    this.studentmodelobj.name=this.formvalue.value.name;
+   this.studentmodelobj.email=this.formvalue.value.email;
+   this.studentmodelobj.mobile=this.formvalue.value.mobile;
+   this.studentmodelobj.city=this.formvalue.value.city;
+
+   this.api.poststudent(this.studentmodelobj).subscribe(
+     res=>{
+     console.log(res)
+     this.formvalue.reset()
+
+     alert("recoard add sucessfuly");
+   },
+  err=>{
+  alert("something went rong");
+}
+myObserver
+)
+ }
 
 }
